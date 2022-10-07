0c879dcb08974c94d130ca5cf0fb4e4ef3cedf42
Akbar Ahmed <70852077+Akbar-Ahmed@users.noreply.github.com>
2022-10-07 15:40:13 +0530

    Add files via upload

---
+++ Block A/addition.py
@@ -0,0 +1,28 @@
+#code 1
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+
+#code 2
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+#code 3
+#Alternate way without variable 
+print('The sum is %.1f' %(float(input('Enter first number: ')) + float(input('Enter second number: '))))
